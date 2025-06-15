546cd7fe2165c27a96127f322a8dd01a13f53a30
Randy Hollaway <33422557+RSHollaway@users.noreply.github.com>
2025-06-14 21:31:11 -0400

    Apply Terraform best practices: capitalize tag key, add variable types, output public IP and DNS.

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,10 @@ output "instance_arn" {
   value = aws_instance.ubuntu.arn
 }
 
+output "instance_public_ip" {
+  value = aws_instance.ubuntu.public_ip
+}
+
+output "instance_public_dns" {
+  value = aws_instance.ubuntu.public_dns
+}
